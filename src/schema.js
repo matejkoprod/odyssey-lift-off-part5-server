cb5926bf02ad3945e0ddd0d68dd76d24d1c295c2
Matej Cummings <matthew.cummings@prodigygame.com>
2022-08-10 19:46:46 +0000

    woop

--- src/schema.js
+++ src/schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    "Fetch a specific track, provided a track's ID"
+    "Fetch a specific track, provided a track's ID ... is good!"
     track(id: ID!): Track!
     "Fetch a specific module, provided a module's ID"
     module(id: ID!): Module!
